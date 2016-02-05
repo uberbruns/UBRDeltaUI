0292a87d2d6845d7e20712f31083233516031d53
uberbruns <karsten@bruns.me>
2016-02-05 17:16:51 +0100

    Add comments

--- UBRDeltaUI/DeltaLib/UBRDelta+Protocols.swift
+++ UBRDeltaUI/DeltaLib/UBRDelta+Protocols.swift
@@ -8,10 +8,17 @@
 
 import Foundation
 
-
+/// This protocol is the foundation for diffing types:
+/// It allows UBRDelta to compare instances by determining
+/// if instances are a) the same, b) the same with changed properties, c) completly different entities.
 public protocol ComparableItem {
     
+    /// The uniqued identifier is used to determine if to instances
+    /// represent the same set of data
     var uniqueIdentifier: Int { get }
+    
+    /// Implement this function to determine how two instances relate to another
+    /// Are they the same, same but with changed data or completly differtent
     func compareTo(other: ComparableItem) -> ComparisonLevel
     
 }
